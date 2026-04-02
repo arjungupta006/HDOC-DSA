50b406761241b8dfb5fd4247416866f37a3c55db
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-03 00:16:15 +0530

    Update q51.c

--- q51.c
+++ q51.c
@@ -1,8 +1,62 @@
-// Question 51
-// Date: 2026-03-23
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return newNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return LCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return LCA(root->right, n1, n2);
+
+    return root;
+}
+
 int main() {
-    printf("Question 51\n");
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* ans = LCA(root, n1, n2);
+
+    if (ans != NULL)
+        printf("%d", ans->data);
+
     return 0;
 }
