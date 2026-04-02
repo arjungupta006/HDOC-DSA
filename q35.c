01f2a6f26d8d535083139046ce63d3637180bf1d
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-02 23:50:56 +0530

    Update q35.c

--- q35.c
+++ q35.c
@@ -1,8 +1,55 @@
-// Question 35
-// Date: 2026-03-07
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Queue {
+    struct Node *front, *rear;
+};
+
+struct Queue* createQueue() {
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(struct Queue* q, int value) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+void printQueue(struct Queue* q) {
+    struct Node* temp = q->front;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
 int main() {
-    printf("Question 35\n");
+    int n, x;
+    scanf("%d", &n);
+
+    struct Queue* q = createQueue();
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(q, x);
+    }
+
+    printQueue(q);
+
     return 0;
 }
