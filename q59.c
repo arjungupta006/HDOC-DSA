07e396dba7abee59bb825d7b56d951a2840f2cf3
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-03 00:23:27 +0530

    Update q59.c

--- q59.c
+++ q59.c
@@ -1,8 +1,69 @@
-// Question 59
-// Date: 2026-03-31
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct Node* build(int inorder[], int postorder[], int inStart, int inEnd, int* postIndex) {
+    if (inStart > inEnd) return NULL;
+
+    int rootVal = postorder[*postIndex];
+    (*postIndex)--;
+
+    struct Node* root = newNode(rootVal);
+
+    if (inStart == inEnd) return root;
+
+    int inIndex = search(inorder, inStart, inEnd, rootVal);
+
+    root->right = build(inorder, postorder, inIndex + 1, inEnd, postIndex);
+    root->left = build(inorder, postorder, inStart, inIndex - 1, postIndex);
+
+    return root;
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
 int main() {
-    printf("Question 59\n");
+    int n;
+    scanf("%d", &n);
+
+    int inorder[n], postorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+
+    int postIndex = n - 1;
+    struct Node* root = build(inorder, postorder, 0, n - 1, &postIndex);
+
+    preorder(root);
+
     return 0;
 }
