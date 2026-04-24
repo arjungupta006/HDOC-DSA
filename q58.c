defe570c1a400bead8f85e2506888952e4130bbd
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:06:44 +0530

    Implement binary tree construction from preorder and inorder

--- q58.c
+++ q58.c
@@ -1,8 +1,69 @@
-// Question 58
-// Date: 2026-03-30
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct Node* build(int preorder[], int inorder[], int inStart, int inEnd, int* preIndex) {
+    if (inStart > inEnd) return NULL;
+
+    int rootVal = preorder[*preIndex];
+    (*preIndex)++;
+
+    struct Node* root = newNode(rootVal);
+
+    if (inStart == inEnd) return root;
+
+    int inIndex = search(inorder, inStart, inEnd, rootVal);
+
+    root->left = build(preorder, inorder, inStart, inIndex - 1, preIndex);
+    root->right = build(preorder, inorder, inIndex + 1, inEnd, preIndex);
+
+    return root;
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
 int main() {
-    printf("Question 58\n");
+    int n;
+    scanf("%d", &n);
+
+    int preorder[n], inorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &preorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    int preIndex = 0;
+    struct Node* root = build(preorder, inorder, 0, n - 1, &preIndex);
+
+    postorder(root);
+
     return 0;
 }
