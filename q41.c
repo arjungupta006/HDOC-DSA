08e3ea56e28d4f2b387c337b718deb800ea465d3
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:01:08 +0530

    Update q41.c

--- q41.c
+++ q41.c
@@ -1,8 +1,64 @@
-// Question 41
-// Date: 2026-03-13
+#include <iostream>
+using namespace std;
 
-#include <stdio.h>
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class Queue {
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = rear = NULL;
+    }
+
+    void enqueue(int x) {
+        Node* temp = new Node(x);
+        if (rear == NULL) {
+            front = rear = temp;
+            return;
+        }
+        rear->next = temp;
+        rear = temp;
+    }
+
+    void dequeue() {
+        if (front == NULL) {
+            cout << -1 << endl;
+            return;
+        }
+        Node* temp = front;
+        cout << front->data << endl;
+        front = front->next;
+        if (front == NULL)
+            rear = NULL;
+        delete temp;
+    }
+};
+
 int main() {
-    printf("Question 41\n");
+    int N;
+    cin >> N;
+    Queue q;
+
+    while (N--) {
+        string op;
+        cin >> op;
+        if (op == "enqueue") {
+            int x;
+            cin >> x;
+            q.enqueue(x);
+        } else if (op == "dequeue") {
+            q.dequeue();
+        }
+    }
     return 0;
 }
