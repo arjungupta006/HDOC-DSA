40aef8850ab3993f21cf975c5d4390343c193180
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:09:29 +0530

    Update q62.c

--- q62.c
+++ q62.c
@@ -1,4 +1,48 @@
-// Question 62
-// Date: 2026-04-04
 #include <stdio.h>
-int main(){return 0;}
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m, i, u, v;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+
+    for(i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    for(i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        newNode = createNode(u);
+        newNode->next = adj[v];
+        adj[v] = newNode;
+    }
+
+    for(i = 0; i < n; i++) {
+        printf("%d: ", i);
+        struct Node* temp = adj[i];
+        while(temp != NULL) {
+            printf("%d ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
