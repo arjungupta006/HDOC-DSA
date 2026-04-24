42ca87eeb924a32905f78ded2558d2696a84dc4a
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 10:59:51 +0530

    Implement priority queue with insert and deleteMin

--- q37.c
+++ q37.c
@@ -1,8 +1,59 @@
-// Question 37
-// Date: 2026-03-09
-
 #include <stdio.h>
+#define MAX 1000
+
+int pq[MAX];
+int size = 0;
+
+void insert(int x) {
+    pq[size++] = x;
+}
+
+int getMinIndex() {
+    if (size == 0) return -1;
+    int minIdx = 0;
+    for (int i = 1; i < size; i++) {
+        if (pq[i] < pq[minIdx])
+            minIdx = i;
+    }
+    return minIdx;
+}
+
+int deleteMin() {
+    if (size == 0) return -1;
+    int idx = getMinIndex();
+    int val = pq[idx];
+    for (int i = idx; i < size - 1; i++)
+        pq[i] = pq[i + 1];
+    size--;
+    return val;
+}
+
+int peek() {
+    if (size == 0) return -1;
+    int idx = getMinIndex();
+    return pq[idx];
+}
+
 int main() {
-    printf("Question 37\n");
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        char op[10];
+        scanf("%s", op);
+
+        if (op[0] == 'i') {
+            int x;
+            scanf("%d", &x);
+            insert(x);
+        } 
+        else if (op[0] == 'd') {
+            printf("%d\n", deleteMin());
+        } 
+        else if (op[0] == 'p') {
+            printf("%d\n", peek());
+        }
+    }
+
     return 0;
 }
