1686d17a6ecc22989c127946b74dc70711a1cf04
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:10:38 +0530

    Update q64.c

--- q64.c
+++ q64.c
@@ -1,4 +1,62 @@
-// Question 64
-// Date: 2026-04-06
 #include <stdio.h>
-int main(){return 0;}
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m, i, u, v, s;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+    int visited[n];
+
+    for(i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    for(i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        newNode = createNode(u);
+        newNode->next = adj[v];
+        adj[v] = newNode;
+    }
+
+    scanf("%d", &s);
+
+    int queue[n], front = 0, rear = 0;
+
+    visited[s] = 1;
+    queue[rear++] = s;
+
+    while(front < rear) {
+        int curr = queue[front++];
+        printf("%d ", curr);
+
+        struct Node* temp = adj[curr];
+        while(temp != NULL) {
+            if(!visited[temp->vertex]) {
+                visited[temp->vertex] = 1;
+                queue[rear++] = temp->vertex;
+            }
+            temp = temp->next;
+        }
+    }
+
+    return 0;
+}
