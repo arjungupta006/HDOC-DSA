80d17d739cee1ed953522637d35501d9124485f5
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:17:41 +0530

    Update q75.c

--- q75.c
+++ q75.c
@@ -1,4 +1,23 @@
-// Question 75
-// Date: 2026-04-17
 #include <stdio.h>
-int main(){return 0;}
+
+int main() {
+    int n, i, j, sum, maxLen = 0;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++) {
+        sum = 0;
+        for(j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum == 0 && (j - i + 1) > maxLen)
+                maxLen = j - i + 1;
+        }
+    }
+
+    printf("%d", maxLen);
+
+    return 0;
+}
