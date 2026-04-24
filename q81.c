4170f966de54b058443a925ab1dd7348a34bceac
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:21:24 +0530

    Update q81.c

--- q81.c
+++ q81.c
@@ -1,4 +1,25 @@
-// Question 81
-// Date: 2026-04-23
 #include <stdio.h>
-int main(){return 0;}
+
+int main() {
+    int n, i, j, temp;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
