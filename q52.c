39f5f962ed814338b60ec6c92395d7b33775dcd7
Arjun Gupta <arjungupta34006@gmail.com>
2026-04-24 11:04:52 +0530

    Update q52.c

--- q52.c
+++ q52.c
@@ -1,8 +1,75 @@
-// Question 52
-// Date: 2026-03-24
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0, j = 1; j < n; i++) {
+        if (nodes[i] != NULL) {
+            if (j < n)
+                nodes[i]->left = nodes[j++];
+            if (j < n)
+                nodes[i]->right = nodes[j++];
+        }
+    }
+
+    return nodes[0];
+}
+
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = LCA(root->left, n1, n2);
+    struct Node* right = LCA(root->right, n1, n2);
+
+    if (left && right) return root;
+
+    return (left != NULL) ? left : right;
+}
+
 int main() {
-    printf("Question 52\n");
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* ans = LCA(root, n1, n2);
+
+    if (ans != NULL)
+        printf("%d", ans->data);
+
     return 0;
 }
